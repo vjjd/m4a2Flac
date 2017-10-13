16d64543431bd918bb01feb412586b0f2d69dc79
ymhtp <ymhtp@ymhtps-MacBook-Air.local>
2017-10-13 19:23:21 +0700

    Implementation Added: path from argv; mkdir for albums

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 const ffmpeg = require('fluent-ffmpeg')
-const { spawn } = require('child_process')
 const fs = require('fs')
-const pathm4a = `/Users/ymhtp/Desktop/ToCowom/Ryo\ Fukui`
-const pathFlac = `/Users/ymhtp/Desktop/Ryo\ Fukui`
+const argv = require('yargs').argv
 
-fs.readdir(pathm4a, (err, list) => {
-    err ? errorHandler(err) :
-    albums = list.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
-    console.log(albums)
+// const m4a_path = `/Users/ymhtp/Desktop/ToCowom/Ryo\ Fukui`
+// const flac_path = `/Users/ymhtp/Desktop/Ryo\ Fukui`
 
-    albums.map((album)=> {
-        fs.readdir(`${pathm4a}/${album}`, (err, list)=> {
-            err ? errorHandler(err) :
-            files = list.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
-            console.log(files)
 
-            files.map((file)=> {
-                new ffmpeg(`${pathm4a}/${album}/${file}`)
-                .fromFormat('m4a')
-                .toFormat('flac')
-                .saveToFile(`${pathFlac}/${album}/${file.replace(/m4a/, 'flac')}`)
-                .on('end', ()=> {
-                    console.log(`End: ${file}`);
-                });
+run()
+
+function run(){
+    fs.readdir(argv.from, (err, list) => {
+        err ? errorHandler(err) :
+        albums = list.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
+        console.log(albums)
+    
+        albums.map((album)=> {
+            fs.readdir(`${argv.from}/${album}`, (err, list)=> {
+                err ? errorHandler(err) :
+                files = list.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
+                console.log(files)
+                
+                fs.mkdirSync(`${argv.to}/${album}`)
+
+                files.map((file)=> {
+                    new ffmpeg(`${argv.from}/${album}/${file}`)
+                    .fromFormat('m4a')
+                    .toFormat('flac')
+                    .saveToFile(`${argv.to}/${album}/${file.replace(/m4a/, 'flac')}`)
+                    .on('end', ()=> {
+                        console.log(`End: ${file}`);
+                    })
+                })  
             })
         })
-    })
-  });
+      });
+}
 
   function errorHandler(err){
     console.log(`Error: ${err}`)
