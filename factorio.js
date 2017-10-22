d60eac0a9f2cc0cf58f5370768a47de1b49f5562
ymhtp <ymhtp@ymhtps-MacBook-Air.local>
2017-10-23 04:29:22 +0700

    Refactoring

--- factorio.js
+++ factorio.js
@@ -1,23 +1,19 @@
-const fs = require('fs')
-//const argv = require('yargs').argv
-const path = require('path')
+// const fs = require('fs')
+// //const argv = require('yargs').argv
+// const path = require('path')
+// const argv = require('yargs').argv
+// const yargs = require('yargs')(['--info']).usage(
+//   'npm start --from="path to music folder" --to="path to outcome" --format="m4a or ape"'
+// )
 
-const argv = require('yargs').argv
-const yargs = require('yargs')(['--info']).usage(
-  'npm start --from="path to music folder" --to="path to outcome" --format="m4a or ape"'
-)
-//   .help('info').argv
+// function lil() {
+//   if (!argv.to || !argv.from || !argv.format) {
+//     return 'lul'
+//   }
+// }
 
-if (!argv.to || !argv.from || !argv.format) {
-  yargs.help('info').argv
+function sum(a, b) {
+  return a + b
 }
-// console.log(argv)
 
-// const m4a_path = `/Users/ymhtp/Desktop/ToCowom/Ryo\ Fukui`
-// const flac_path = `/Users/ymhtp/Desktop/Ryo\ Fukui`
-
-// let arrFiles = fs.readdirSync(argv.from).filter(f => !(/(^|\/)\.[^\/\.]/g).test(f))
-
-// arrFiles.map(f => {
-//     path.extname(f) ? console.log('lul') : console.log('kek')
-// })
+module.exports = sum
--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const yargs = require('yargs')(['--info']).usage(
   'node index.js --from="path to music folder" --to="path to outcome" --format="m4a or ape"'
 )
 
-!argv.to || !argv.from || !argv.format ? yargs.help('info').argv : sortContent()
+/**
+ * Check parameters
+ */
+if (!argv.to || !argv.from || !argv.format) {
+  yargs.help('info').argv
+} else {
+  sortContent()
+}
 
 /**
  * Sort content into songs or albums
@@ -18,9 +25,11 @@ function sortContent() {
     .filter(f => !/(^|\/)\.[^\/\.]/g.test(f))
   let files = contents.filter(f => regexpFormat.test(path.extname(f)))
 
-  !files.length
-    ? pullFromAlbums(contents.filter(f => !path.extname(f)))
-    : convertSongs(files)
+  if (files.length) {
+    pullFromAlbums(contents.filter(f => !path.extname(f)))
+  } else {
+    convertSongs(files)
+  }
 }
 
 /**
@@ -30,13 +39,17 @@ function sortContent() {
 function pullFromAlbums(albums) {
   albums.map(album => {
     fs.readdir(`${argv.from}/${album}`, (err, list) => {
-      err ? errorHandler(err) : (songs = list.filter(s => regexpFormat.test(s)))
+      if (err) {
+        errorHandler(err)
+      } else {
+        songs = list.filter(s => regexpFormat.test(s))
+      }
 
       fs.mkdirSync(`${argv.to}/${album}`)
 
       console.log(`#Converting ${songs}`)
 
-      songs.map(file => {
+      songs.forEach(file => {
         new ffmpeg(`${argv.from}/${album}/${file}`)
           .fromFormat(argv.format)
           .toFormat('flac')
@@ -57,7 +70,7 @@ function pullFromAlbums(albums) {
  */
 function convertSongs(songs) {
   console.log(`#Converting ${songs}`)
-  songs.map(file => {
+  songs.forEach(file => {
     new ffmpeg(`${argv.from}/${file}`)
       .fromFormat(argv.format)
       .toFormat('flac')
@@ -69,7 +82,7 @@ function convertSongs(songs) {
 }
 
 /**
- * 
+ * Handle a error
  * @param {*error} err 
  */
 function errorHandler(err) {
